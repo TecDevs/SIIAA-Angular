6a5474fd32c4893f11ed1542ee17b2799532d773
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-09-06 16:20:07 -0500

    correcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,7 +65,7 @@ import { ConfiguracionComponent } from './components/shared/configuracion/config
     RecursosHumanosComponent,
     ReportesComponent,
     NavbarInicioComponent,
-    ConfiguracionComponent
+    ConfiguracionComponent,
     GraficarAreasComponent,
     GraficarTotalComponent
   ],
